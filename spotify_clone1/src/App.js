35f3eac5c628c9c3c49e7e49963ee2b49609f4f5
moeez <moeez.mustafa@outlook.com>
2021-05-06 13:05:10 -0400

    25:00

---
+++ spotify_clone1/src/App.js
@@ -0,0 +1,19 @@
+import React  from "react";
+import './App.css';
+import Login from "./login";
+
+
+function App() {
+  return (
+    <div className="App">
+        
+        <p>
+          Hey this is moez and i am programming
+        </p>
+
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ spotify_clone1/src/login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./login.css";
+
+
+function login()
+{
+    return (
+        //BEM
+        <div classname = "login">
+            <h1>Hi this a programmer here on this side</h1>
+
+        </div>
+    )
+}
+
+export default Login ;
